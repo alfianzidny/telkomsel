47015286bb9e0ffe1a828af0b86c81f3803cdef1
alfianzidny <69819216+alfianzidny@users.noreply.github.com>
2020-09-13 02:20:59 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Inject FREE NET INDONESIA
+Simpel Server Kak
+
+git clone https://github.com/alfianzidny/telkomsel.git
+cd telkomsel
+sh install.sh
+
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!usr/bin/bash
+#Alfian Zidny
+
+b='\033[34;1m'
+g='\033[32;1m'
+
+clear
+
+figlet -f slant "Inject Free NET INDO" | lolcat
+echo $g"-----------------------------------"
+echo $b "Uploader By ALFIAN ZIDNY"
+echo $b "Https://github.com/alfianzidny"
+echo $g "----------------------------------"
+sleep 2
+echo "Installing.."
+sleep 4
+apt update && apt upgrade -y
+apt install curl
+apt install python2
+clear
+python2 main.py
+
+
+
+#Mau Ngapain Kak ?
